603766ce9fa795723991e03d157ff6277e04e922
deepanshu tiwari <deepanshut@geekyants.com>
2023-03-20 12:54:46 +0530

    fix: mongoose connect call

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = new mongoose.connect(process.env.MONGO_URI);
+  const conn = await mongoose.connect(process.env.MONGO_URI);
 
   console.log(`MongoDB connected ${conn.connection.host}`.cyan.underline.bold);
 };
