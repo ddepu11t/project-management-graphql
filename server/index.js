55463d360d9d9bba78b49781e7ffc5ddebcceb0c
deepanshu tiwari <deepanshut@geekyants.com>
2023-03-20 12:54:17 +0530

    feat: call connectDB to connect to Db and imports colors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const port = process.env.PORT || 4000;
 const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+
 const app = express();
+
+connectDB();
 
 app.use(
   "/graphql",
