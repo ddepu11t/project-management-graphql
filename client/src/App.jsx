862addae42a41f200c2d2d042c0621760d6df11c
deepanshu tiwari <deepanshut@geekyants.com>
2023-03-20 18:34:28 +0530

    chore: apollo config , create client wrap in Provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import Header from "./components/Header";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container"></div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container"></div>
+      </ApolloProvider>
     </>
   );
 }
