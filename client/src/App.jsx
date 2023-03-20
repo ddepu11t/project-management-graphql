b73c26fa670746f163c2d0ba8e7ad80b228bfeb0
deepanshu tiwari <deepanshut@geekyants.com>
2023-03-20 18:26:39 +0530

    feat: create header and use it in the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Header />
+      <div className="container"></div>
+    </>
   );
 }
 
