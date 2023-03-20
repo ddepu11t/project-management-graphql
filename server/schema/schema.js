c4c8fd9b659a5c7bacf80f2d5d22ee143bedc669
deepanshu tiwari <deepanshut@geekyants.com>
2023-03-20 11:51:05 +0530

    feat: query for  the client list

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,6 +5,7 @@ const {
   GraphQLID,
   GraphQLString,
   GraphQLSchema,
+  GraphQLList,
 } = require("graphql");
 
 // Client Type
@@ -21,6 +22,13 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parant, args) {
+        return clients;
+      },
+    },
+
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
