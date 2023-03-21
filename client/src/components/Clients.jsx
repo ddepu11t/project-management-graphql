ba94ca538497b59103b44f4be91d4fc97bd24488
deepanshu tiwari <deepanshut@geekyants.com>
2023-03-21 11:16:56 +0530

    feat create get clients query and use useQuery.

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,5 +1,28 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_CLIENTS = gql`
+  query getClients {
+    clients {
+      id
+      name
+      email
+      phone
+    }
+  }
+`;
+
 const Clients = () => {
-  return <div>Clients</div>;
+  const { loading, error, data } = useQuery(GET_CLIENTS);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Some thing went wrong</p>;
+  }
+
+  return <>{!loading && !error ? <h1>Clients</h1> : null}</>;
 };
 
 export default Clients;
