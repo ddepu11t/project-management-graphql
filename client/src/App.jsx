add99889e50a6e0f1399df35879376b8196fa1f7
deepanshu tiwari <deepanshut@geekyants.com>
2023-03-21 14:00:24 +0530

    refactor: add typePolicies in InMemoryCache

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { Clients, Header } from "./components";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
