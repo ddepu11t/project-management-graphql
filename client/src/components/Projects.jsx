f32c6f488b7f200359760fb162f0ffb463ea01b4
deepanshu tiwari <deepanshut@geekyants.com>
2023-03-24 14:46:50 +0530

    style: add marging top

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
   return (
     <>
       {data.projects.length > 0 ? (
-        <div className="row">
+        <div className="row mt-4">
           {data.projects.map((project) => {
             return <ProjectCard key={project.id} project={project} />;
           })}
