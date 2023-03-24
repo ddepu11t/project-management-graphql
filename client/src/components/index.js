f9c596fc240ba1ce80aa41ba661e473d834ba740
deepanshu tiwari <deepanshut@geekyants.com>
2023-03-24 12:55:31 +0530

    chore: export AddClientsModal

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Header } from "./Header";
 export { default as Clients } from "./Clients";
 export { default as ClientRow } from "./ClientRow";
 export { default as Spinner } from "./Spinner";
+export { default as AddClientsModal } from "./AddClientsModal";
