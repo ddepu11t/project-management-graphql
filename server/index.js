432f5fe31a6dd42094add6ea65074d2c4fb556a9
deepanshu tiwari <deepanshut@geekyants.com>
2023-03-30 14:37:41 +0530

    config: for deployment

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const { graphqlHTTP } = require("express-graphql");
 const port = process.env.PORT || 4000;
 const schema = require("./schema/schema");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -19,6 +20,20 @@ app.use(
     graphiql: process.env.NODE_ENV === "development",
   })
 );
+
+// deployment
+
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
+
+// deployment
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
